b02dc7f679eb78a8d9889d85a3234aead0ea873c
M2V0A02 <volodin.mixa2015@yandex.ru>
2022-07-22 19:47:23 +0500

    rename product to message

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,7 +48,7 @@ class User extends Authenticatable
         $this->attributes['password'] = Hash::make($password);
     }
 
-    public function products()
+    public function message()
     {
         return $this->hasMany(Message::class);
     }
